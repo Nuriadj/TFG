287aa7c4b65121532e9f6a47abed5e1d27f92595
Nuriadj <nuria.dzjrc@gmail.com>
2022-02-06 12:44:29 +0100

    first csv generator

--- codigo_sensores/LDR_data_lgpio.py
+++ codigo_sensores/LDR_data_lgpio.py
@@ -1,8 +1,18 @@
 import lgpio
 import numpy as np
 import csv
+import time
+import sys
+
 
 LDR= 4
+
+if (len(sys.argv) != 2):
+    error=""
+    error=error+"Error: wrong number of arguments, execute as: \npython "+sys.argv[0]+" csv_name"
+    sys.exit(error)
+
+csv_name= sys.argv[1]+".csv"
 
 h= lgpio.gpiochip_open(0)
 lgpio.gpio_claim_input(h, LDR)
@@ -13,7 +23,9 @@ muestras= [] #array que contiene los valores del sensor
 while len(muestras) < total_v:
 
     state= lgpio.gpio_read(h, LDR)
+    muestras.append(state)
     print("STATE", state)
+    time.sleep(0.1)
     
 datos= np.array(muestras)
-np.savetxt("Muestras_LDR.csv",data,delimiter=",",fmt='%s')
+np.savetxt(csv_name,datos,delimiter=",",fmt='%s')
