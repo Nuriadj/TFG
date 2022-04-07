753f5bc418bb5b258a9be6549205e0011cd274ea
Nuriadj <nuria.dzjrc@gmail.com>
2022-04-07 18:12:42 +0200

    Leer otros porcentajes del total de kdd_cup99

--- kdd_cup99/Download_dataSet.py
+++ kdd_cup99/Download_dataSet.py
@@ -8,6 +8,7 @@ import sklearn
 from sklearn import datasets
 import pandas as pd
 import numpy
+import random
 
 
 # In[2]:
@@ -20,9 +21,17 @@ import numpy
 # In[ ]:
 
 
-dataset= pd.read_csv('/home/nuria/Documents/TFG/sklearnDataSet/kddcup.csv')
+n= 4940200 # 494020 es el 10% del total, luego el fichero total tendra 4940200 lineas
+s= 2470100 # Leer solo el 50%
+skip= sorted(random.sample(range(n),n-s))
+dataset = pd.read_csv('/home/nuriadj/Documents/TFG/kdd_cup99/kddcup.data', skiprows=skip)
+print("Leido un: ",(len(dataset)*100)/n,"% del csv")
+headers = list(dataset.columns.values)
+
+#dataset= pd.read_csv('/home/nuriadj/Documents/TFG/kdd_cup99/kddcup.data')
 
 #change Multi-class to binary-class
 dataset['normal.'] = dataset['normal.'].replace(['back.', 'buffer_overflow.', 'ftp_write.', 'guess_passwd.', 'imap.', 'ipsweep.', 'land.', 'loadmodule.', 'multihop.', 'neptune.', 'nmap.', 'perl.', 'phf.', 'pod.', 'portsweep.', 'rootkit.', 'satan.', 'smurf.', 'spy.', 'teardrop.', 'warezclient.', 'warezmaster.'], 'attack')
-dataSet.to_csv('kddcup_Binary.csv')
+
+dataset.to_csv('kddcup_50_perBinary.csv', header= False, index= False)
 
